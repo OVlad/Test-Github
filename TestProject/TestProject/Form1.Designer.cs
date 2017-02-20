a47a0856f519b555c96bce84bec3eedcab9573d6
Vlad Oncioiu <96vladoncioiu@gmail.com>
2017-02-20 21:37:13 +0200

    Moved the only button again >(

--- TestProject/TestProject/Form1.Designer.cs
+++ TestProject/TestProject/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(308, 87);
+            this.button1.Location = new System.Drawing.Point(169, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(157, 23);
             this.button1.TabIndex = 0;
